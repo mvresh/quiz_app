4088e9520230b1b64fec81c776032aa680f88bb1
Varun Resh Maddali <mvresh@gmail.com>
2019-10-04 19:31:27 +0530

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,25 @@ class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text("Your content here"),
+      child: Column(
+        children: <Widget>[
+          Expanded(flex:4,child: Center(child: Container(child:Text('Question here')))),
+          Expanded(flex:2,child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(color:Colors.blue,onPressed: (){}, child: Text('True',style: TextStyle(color: Colors.white),),),
+              ),
+              SizedBox(width: 30,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(color:Colors.blue,onPressed: (){}, child: Text('False',style: TextStyle(color: Colors.white))),
+              )
+            ],
+          )),
+        ],
+      ),
     );
   }
 }
